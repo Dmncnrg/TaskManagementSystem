2a5db179476ebe88dd0908b1767cb6e7c1fd6f29
Dominic Narag <60565226+Dmncnrg@users.noreply.github.com>
2021-07-10 23:32:59 +0800

    database_table_content

--- application/models/Task_Model.php
+++ application/models/Task_Model.php
@@ -5,6 +5,15 @@ class Task_Model extends CI_Model{
         $query = $this->db->get('tbl_user'); //change niyo lang table name if user or task table
         return $query->num_rows();
     }
+    public function fetch_data($limit,$offset){ //Pagination (Homepage)
+        $this->db->limit($limit);
+        $this->db->offset($offset);
+        $result = $this->db->get('tbluser')->result();
+        return $result;
+    }
+    public function CountAll(){ //Pagination function
+        return $this->db->get('tbluser')->num_rows();
+    }
     public function show_data(){ //pang show ng laman ng table
         $query = $this->db->get('tbl_user'); //change niyo lang table name if user or task table
         return $query->result();
--- application/views/table_content.php
+++ application/views/table_content.php
@@ -8,11 +8,32 @@
 </head>
 <body>
     <?php
-    
+    // Uncomment niyo na lang yung need niyo
+
+    //      USER TABLE
+    /*
     foreach($info as $i){
-        print($i);
+        echo "id: ".$i->id."<br>";
+        echo "fname: ".$i->fname."<br>";
+        echo "lname: ".$i->lname."<br>";
+        echo "username: ".$i->username."<br>";
+        echo "password: ".$i->password."<br>";
+        echo "email: ".$i->email."<br>";
+        echo "profile_pic: ".$i->profile_pic."<br>";
+    }*/
+
+    //      TASK TABLE
+    /*
+    foreach($info as $i){
+        echo "id: ".$i->id."<br>";
+        echo "user_id: ".$i->user_id."<br>";
+        echo "task: ".$i->task."<br>";
+        echo "description: ".$i->description."<br>";
+        echo "task_start: ".$i->task_start."<br>";
+        echo "task_due: ".$i->task_due."<br>";
+        echo "task_status: ".$i->profile_pic."<br>";
     }
-    
+    */
     ?>
 </body>
 </html>
